07299efb438c76232381048511d8949dcdb13e4f
Parskarl <pasci17.3.2006@gmail.com>
2022-09-30 11:45:23 +0200

    1. Commit Erstellt

---
+++ src/net/htlgkr/SaurerP190201/hue3Saurer/Main.java
@@ -0,0 +1,14 @@
+package net.htlgkr.SaurerP190201.hue3Saurer;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void readfile()
+    {
+        
+    }
+}
