68de46269584e24158f22ce42030dd338afe65b4
Parskarl <pasci17.3.2006@gmail.com>
2022-10-03 19:36:18 +0200

    5. Commit Main 4 Fertig

--- src/net/htlgkr/SaurerP190201/hue3Saurer/Main4.java
+++ src/net/htlgkr/SaurerP190201/hue3Saurer/Main4.java
@@ -6,7 +6,7 @@ public class Main4
 {
     public static void main(String[] args)
     {
-        final int result = IntStream.of(1,2,3,4,5,6,7,8,9,10).filter(x -> x%2==0).map()a
+        final int result = IntStream.of(1,2,3,4,5,6,7,8,9,10).filter(x -> x%2!=0).map(x -> x*x).reduce(0,(b1, b2)-> b1+b2);
+        System.out.println(result);
     }
-    }
-
+}
